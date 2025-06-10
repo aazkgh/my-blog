4c56e62ca5e18a766305b06fcd6c205c9370b7cd
고가형 <101045330+aazkgh@users.noreply.github.com>
2025-06-10 17:39:01 +0900

    Init: 린트 세팅 (#5)
    * init: pnpm/vanila extract 설치
    
    * init: 린트 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
+/* @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withVanillaExtract = createVanillaExtractPlugin();
+
+export default withVanillaExtract(nextConfig);
