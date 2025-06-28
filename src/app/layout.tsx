0cd68e02fca6e5df71e72605b1f0dd1e2a705b8a
aazkgh <gahyungko@gmail.com>
2025-06-28 17:09:53 +0900

    feat: 메타 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,25 @@ import folderIcon from "@/../public/images/folderImg.png";
 import homeIcon from "@/../public/images/comrImg.png";
 import * as styles from "@/styles/layout.css";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
+    <>
+    <Head>
+      <title>aablogz</title>
+      <meta name="description" content="주니어 개발자가 감자에서 싹을 틔우는 중..." />
+      <meta name="keywords" content="개발자, 개발, 블로그, 프론트엔드, 백엔드, 리액트, Next.js, 성장" />
+      <meta name="author" content="고가형" />
+      <meta name="robots" content="index, follow" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Ordinary Girl" />
+      <meta property="og:title" content="Ordinary Girl" />
+      <meta property="og:description" content="주니어 개발자, 고가형의 블로그입니다" />
+      <meta property="og:image" content="https://aablogz.vercel.app/images/kakao_thumbnail.png" />
+      <meta property="og:url" content="https://aablogz.vercel.app/" />
+    </Head>
     <html lang="ko">
       <body className={styles.desktop}>
         <div className={styles.folders}>
@@ -27,5 +43,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         {children}
       </body>
     </html>
+    </>
   );
 }
