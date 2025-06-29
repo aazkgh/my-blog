7028ceb280e46f048ae2112228c5190ac14a239d
aazkgh <gahyungko@gmail.com>
2025-06-29 14:32:36 +0900

    fix: 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,34 @@ import folderIcon from "@/../public/images/folderImg.png";
 import homeIcon from "@/../public/images/comrImg.png";
 import * as styles from "@/styles/layout.css";
 import Link from "next/link";
+import type { Metadata } from 'next'
+ 
+
+export const metadata: Metadata = {
+  title: "Ordinary Girl",
+  description: "주니어 개발자가 감자에서 싹을 틔우는 중...",
+  keywords: ["개발자", "개발", "블로그", "프론트엔드", "백엔드", "리액트", "Next.js", "성장"],
+  authors: [{ name: "고가형" }],
+  robots: "index, follow",
+  icons: {
+    icon: "/favicon.ico"
+  },
+  openGraph: {
+    type: "website",
+    siteName: "Ordinary Girl",
+    title: "Ordinary Girl",
+    description: "주니어 개발자, 고가형의 블로그입니다",
+    url: "https://aablogz.vercel.app/",
+    images: [
+      {
+        url: "https://aablogz.vercel.app/images/kakao_thumbnail.png",
+        width: 1200,
+        height: 600,
+        alt: "aablogz 썸네일"
+      }
+    ]
+  }
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
